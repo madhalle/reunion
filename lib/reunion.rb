258de31067868025a73cc468f41be8086578c270
Mad Hallé <mad.halle@icloud.com>
2021-01-11 08:51:32 -0700

    iteration 3 walkthrough

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -9,4 +9,29 @@ class Reunion
   def add_activity(activity)
     @activities.push(activity)
   end
+
+  def total_cost
+    @activities.sum do |activity|
+      activity.total_cost
+    end
+  end
+
+  def breakout
+    breakout = Hash.new(0)
+    @activities.each do |activity|
+      activity.owed.each do |participant, owed|
+        breakout[participant] += owed
+      end
+    end
+    breakout
+  end
+
+  def summary
+    final_string = ""
+    breakout.each do |name, owed|
+       # "#{name}: #{owed}\n"
+       final_string += "#{name}: #{owed}\n"
+    end
+    final_string[0..-2]
+  end
 end
--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -18,4 +18,50 @@ class ReunionTest < Minitest::Test
     @reunion.add_activity(@activity_1)
     assert_equal [@activity_1], @reunion.activities
   end
+
+  def test_total_cost
+    @activity_1.add_participant("Maria", 20)
+
+    @activity_1.add_participant("Luther", 40)
+
+    @reunion.add_activity(@activity_1)
+
+    assert_equal 60, @reunion.total_cost
+  end
+
+  def test_breakout_and_summary
+    @activity_1.add_participant("Maria", 20)
+    @activity_1.add_participant("Luther", 40)
+
+    @reunion.add_activity(@activity_1)
+
+    activity_2 = Activity.new("Drinks")
+    activity_2.add_participant("Maria", 60)
+    activity_2.add_participant("Luther", 60)
+    activity_2.add_participant("Louis", 0)
+
+    @reunion.add_activity(activity_2)
+    assert_equal 180, @reunion.total_cost
+
+    expected = {"Maria" => -10,
+                "Luther" => -30,
+                "Louis" => 40}
+    assert_equal expected, @reunion.breakout
+
+    summary_string = "Maria: -10\nLuther: -30\nLouis: 40"
+    assert_equal summary_string, @reunion.summary
+    puts @reunion.summary
+  end
 end
+#
+#
+#
+
+# pry(main)> reunion.summary
+# # => "Maria: -10\nLuther: -30\nLouis: 40"
+#
+# pry(main)> puts reunion.summary
+# Maria: -10
+# Luther: -30
+# Louis: 40
+# ```
